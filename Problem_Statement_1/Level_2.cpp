9610802bb612af862e02ba8d1090ae040257bea5
abhimanyu-oo <abhi.13.09.2007@gmail.com>
2025-08-19 19:30:05 +0530

    created level 2 and renamed level1

---
+++ Problem_Statement_1/Level_2.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    char Opt;
+    cout << "===Caeser Cipher Program===\n";
+    cout << "This Program offers to both encode as well as decode caeser cipher by user determined shifting of code.\n";
+    cout << "To Encode a message : E\n";
+    cout << "To Decode a message : D\n";
+    cout << "For Smart Decoding a the message : S\n";
+    cout << "Which one do you wish to use : ";
+    cin >> Opt;
+    cin.ignore();
+
+
+    return 0;
+}
